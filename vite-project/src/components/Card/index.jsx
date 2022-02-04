157913ddc14acf366ca728e1dd8a5e8a046c2c5e
gribdesbois <gribdesbois@gmail.com>
2022-02-04 13:37:18 +0100

    defaultProps

--- vite-project/src/components/Card/index.jsx
+++ vite-project/src/components/Card/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import DefaultPicture from '../../assets/profile.webp'
 
 const Card = ({ label, title, picture }) => (
   <div style={{ display: 'flex', flexDirection: 'column', padding: '15px' }}>
@@ -10,9 +11,14 @@ const Card = ({ label, title, picture }) => (
 )
 
 Card.propTypes = {
-  label: PropTypes.string,
-  title: PropTypes.string,
-  picture: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+}
+Card.defaultProps = {
+  label: 'Some shiny label',
+  title: 'Default Title',
+  picture: DefaultPicture,
 }
 
 export default Card
