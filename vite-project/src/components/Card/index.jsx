193c253e78383ce62dc41824fc79794d29db2b7e
gribdesbois <gribdesbois@gmail.com>
2022-02-04 13:13:07 +0100

    card rafcpe

---
+++ vite-project/src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Card = ({ label, title, picture }) => (
+  <div style={{ display: 'flex', flexDirection: 'column', padding: '15px' }}>
+    <span>{label}</span>
+    <img src={picture} alt="freelance" height={80} width={80} />
+    <span>{title}</span>
+  </div>
+)
+
+Card.propTypes = {}
+
+export default Card
