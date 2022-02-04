445df20a08254ca4fd142c2caa67dfe21fdf2013
gribdesbois <gribdesbois@gmail.com>
2022-02-04 13:25:12 +0100

    Freelance with dummy Profiles

--- vite-project/src/pages/Freelances/index.jsx
+++ vite-project/src/pages/Freelances/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import DefaultPicture from '../../assets/profile.webp'
+import Card from '../../components/Card'
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'DevOp',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developer frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jane Biche',
+    jobTitle: 'Developer fullstack',
+    picture: DefaultPicture,
+  },
+]
 
 function Freelances() {
   return (
     <div>
       <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+      {freelanceProfiles.map((profile, index) => {
+        const { name, jobTitle, picture } = profile
+        return (
+          <Card
+            key={`${name}-${index}`}
+            label={jobTitle}
+            picture={picture}
+            title={name}
+          />
+        )
+      })}
     </div>
   )
 }
