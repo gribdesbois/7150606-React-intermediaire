d8adfa87078d39b541314eb092957be5edbc361e
gribdesbois <gribdesbois@gmail.com>
2022-02-04 13:56:00 +0100

    passing props in styled components. i love the future..

--- vite-project/src/components/Header/index.jsx
+++ vite-project/src/components/Header/index.jsx
@@ -7,12 +7,18 @@ const StyledLink = styled(Link)`
   color: #8186a0;
   text-decoration: none;
   font-size: 18px;
+  ${({ props }) =>
+    props?.$isFullLink &&
+    `color: white; border-radius: 30; background-color: #5843E4;`}
 `
 function Header() {
   return (
     <nav>
       <StyledLink to={'/'}>Accueil</StyledLink>
-      <StyledLink to={'/survey/1'}>Questionnaire</StyledLink>
+      <StyledLink to={'/survey/1'} $isFullLink>
+        {/* //! $ is only required when passing to React Component */}
+        Faire le test{/* //! if plain html u can pass isFullLink  */}
+      </StyledLink>
       <StyledLink to={'/freelances'}>Profils</StyledLink>
     </nav>
   )
